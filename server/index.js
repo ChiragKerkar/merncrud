27175cef97d1dc2d7efd37aad2e19d469a64aba5
chirag <kerkarc509@gmail.com>
2024-01-01 14:04:15 +0530

    vercel-upload

--- server/index.js
+++ server/index.js
@@ -4,10 +4,17 @@ const cors = require('cors')
 const UserModel = require('./models/Users')
 
 const app = express()
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json())
 
-mongoose.connect("mongodb://127.0.0.1:27017/crud")
+// mongoose.connect("mongodb://127.0.0.1:27017/crud")
+mongoose.connect("mongodb+srv://kerkarc509:gZLizksPk30DJJR0@todo-list-db.kojq7vu.mongodb.net/?retryWrites=true&w=majority")
 
 app.get("/",(req, res) => {
     UserModel.find({})
